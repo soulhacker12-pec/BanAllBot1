4ff2b1b70455a37bc622f0f019a654f722359120
soulhacker12-pec <155738175+soulhacker12-pec@users.noreply.github.com>
2024-04-20 18:28:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     TOKEN=""
     SUDO = list(int(i) for i in os.environ.get("SUDO", "").split(" "))
     START_IMG="https://telegra.ph/file/50df257b98a528414de84.jpg"
-    BOT_ID=
-    BOT_USERNAME=""
-    BOT_NAME=""
+    
+    
+    
